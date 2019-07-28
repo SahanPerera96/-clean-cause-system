ab2d55be5876d7d45bebf34edc24666809753cde
Sahan Perera <Sahan13perera>
2019-07-29 00:56:09 +0530

    first Commit

---
+++ mobile-service/app/src/main/java/com/clean/cause/cleancause/main/ApplicationMain.java
@@ -0,0 +1,15 @@
+package com.clean.cause.cleancause.startup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.clean.cause.cleancause.R;
+
+public class ApplicationMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application_main);
+    }
+}
